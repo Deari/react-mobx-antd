7f4f3c7399b355c40caffe8bc6d63453b5c87ac9
lizhuo <lizhuo@liepin.com>
2019-12-23 13:32:42 +0800

    refector

--- v5/src/stores/AppStore/index.js
+++ v5/src/stores/AppStore/index.js
@@ -1,4 +1,3 @@
 import { observable, computed, action, runInAction, toJS } from 'mobx';
-import service from '../../services/DeepInterviewReportService';
 class AppStore {}
 export default new AppStore();
